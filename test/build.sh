387dd445d6b8d31e812a312525c2b9e6680663f6
Ryan Lovelett <ryan.a.lovelett@saic.com>
2011-07-21 16:48:23 -0500

    Made more tests compatible with Windows.

--- test/build.sh
+++ test/build.sh
@@ -1,7 +1,7 @@
 LINK="../lib/libffmpeg_fas.so -lm -lz "
 gcc ./dump_frames/dump_frames.c -I.. $LINK -o dump_frames
 gcc ./dump_keyframes/dump_keyframes.c -I.. $LINK -o dump_keyframes
-gcc show_seek_table.c -I.. $LINK -o show_seek_table
-gcc seek_test.c -I.. $LINK -o seek_test
+gcc ./show_seek_table/show_seek_table.c -I.. $LINK -o show_seek_table
+gcc ./seek_test/seek_test.c -I.. $LINK -o seek_test
 gcc external_seek_test.c -I.. $LINK -o external_seek_test
 gcc generate_seek_table.c -I.. -I../ffmpeg/ ../ffmpeg/libavformat/libavformat.a ../ffmpeg/libavutil/libavutil.a ../ffmpeg/libavcodec/libavcodec.a -lm -lz ../lib/libffmpeg_fas.so -o generate_seek_table
